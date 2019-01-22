0f63bc6afb81eaa2e6e58729299396f1bcb4b0f5
Ana Zorro <al.petingazorro@gmail.com>
2019-01-22 13:16:17 +0000

    Ana's Class

---
+++ CO453_AppProgramming/Ana.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CO453_AppProgramming
+{
+    /// <summary>
+    /// Author: Ana Zorro
+    /// 
+    /// This class is just used to try things out!
+    /// </summary>
+    public class Ana
+    {
+        /// <summary>
+        /// This method writes out a title for an application
+        /// </summary>
+        /// <param name="title">This is the Application Title</param>
+        public static void WriteTitle(string title)
+        {
+            Console.WriteLine("==================================================");
+            Console.WriteLine("Ana's " + title);
+            Console.WriteLine("==================================================");
+        }
+    }
+}
--- CO453_AppProgramming/Program.cs
+++ CO453_AppProgramming/Program.cs
@@ -6,13 +6,35 @@ using System.Threading.Tasks;
 
 namespace CO453_AppProgramming
 {
+    /// <summary>
+    /// Author: Ana Zorro
+    /// 
+    /// This class will run all the console app examples
+    /// required for module CO453 Applications Programming
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Ana!");
+
+            //classes are used to create objects. ana is an object which is an instance of the Ana class.
+            //i can have ana1 and ana2 that are intances of Ana class
+            //Create three new objects from the class
+
+            Ana ana = new Ana();
+            Ana ana1 = new Ana();
+            Ana ana2 = new Ana();
+
+            //ana1.WriteTitle("My Wonder Application");
+            //ana2.WriteTitle("My Wizard Application");
+
+            //look at the two comments below
+            //if it is a static, it can be accessed by using the class instead of object (ana, ana1, ana2)
+            //so now I can use the class directly
+
+            Ana.WriteTitle("My Wonder Application");
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
